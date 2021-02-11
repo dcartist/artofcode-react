ebc6982052100354f91420154c6d395296825312
DCartist <pbannerman13@gmail.com>
2021-02-10 22:06:27 -0500

    adding animation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
+import "./css/an.css"
 import './App.css';
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={process.env.PUBLIC_URL + '/images/angel-01.svg'} className="floating"/>
+      <h1>Art of Code</h1>
       </header>
     </div>
   );
